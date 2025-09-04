6d6a2992e098f75b80c04f913bd7282434c4fde6
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-09-04 09:48:31 +0300

    added clerk for authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return (
-   <div>
-	<h1>Hom page</h1>
-   </div>
-  );
+	return (
+		<div>
+			<SignedOut>
+				<SignInButton mode="modal">
+					<button className="bg-red-500 rounded-xl p-1">
+						Sign In
+					</button>
+				</SignInButton>
+			</SignedOut>
+			<SignedIn>
+				<UserButton />
+			</SignedIn>
+		</div>
+	);
 }
