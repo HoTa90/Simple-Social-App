ab95a064d584527c59061c9ca0afdea5fba1d36c
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-09-04 10:54:07 +0300

    added navbar with desktop and mobile comps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
-import { Button } from "@/components/ui/button";
-import ToggleMode from "@/components/ToggleMode";
-import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 
 export default function Home() {
 	return (
 		<div className="m-4">
-			<SignedOut>
-				<SignInButton mode="modal">
-					<Button>Sign In</Button>
-				</SignInButton>
-			</SignedOut>
-			<SignedIn>
-				<UserButton />
-			</SignedIn>
-			<ToggleMode />
-					<Button variant={"secondary"}>Second button</Button>
+	<h1>Home Page</h1>
+
 		</div>
 	);
 }
